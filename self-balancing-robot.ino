0a7ddac92c69775ca4826483773b0ff9a646839b
nvdien <66367482+x1c9@users.noreply.github.com>
2025-05-06 09:45:00 +0700

    v6

--- self-balancing-robot.ino
+++ self-balancing-robot.ino
@@ -15,9 +15,9 @@ float dt = 0.004f;            // Mặc định 4ms
 float error = 0.0f;
 float integral = 0.0f;
 float derivative = 0.0f;
-float kp = 100.0f;
-float ki = 2.0f;
-float kd = 1.0f;
+float kp = 500.0f;
+float ki = 0.0f;
+float kd = 0.0f;
 float throttle = 0.0f;
 float setpoint = 0.0f;        // Góc mong muốn (thường là 0 độ)
 float lastInput = 0.0f;       // Lưu giá trị input (KalmanAnglePitch) trước đó
@@ -25,25 +25,24 @@ float lastTime = 0.0f;        // Lưu thời gian tính toán PID trước đó
 float sampleTime = 0.004f;    // Thời gian lấy mẫu 4ms (250Hz)
 const float MICROSECOND = 1000000.0f;
 const float MIN_DT = 0.001f;  // Thời gian tối thiểu 1ms
-const float MAX_ANGLE = 25.0f; // Ngưỡng góc nghiêng tối đa
+const float MAX_ANGLE = 35.0f; // Ngưỡng góc nghiêng tối đa
 const float MIN_PWM = 5.0f;   // Ngưỡng PWM tối thiểu
 float max_integral;
 float pwm;
 
-float RateRoll, RatePitch, RateYaw;
-float RateCalibrationRoll, RateCalibrationPitch, RateCalibrationYaw;
+float RatePitch;
+float RateCalibrationPitch;
 int RateCalibrationNumber;
 float AccX, AccY, AccZ;
-float AngleRoll, AnglePitch;
+float AnglePitch;
 uint32_t LoopTimer;
-float KalmanAngleRoll = 0, KalmanUncertaintyAngleRoll = 2*2;
-float KalmanAnglePitch = 0, KalmanUncertaintyAnglePitch = 2*2;
+float KalmanAnglePitch = 0, KalmanUncertaintyAnglePitch = 4;
 float Kalman1DOutput[] = {0, 0};
 
 void kalman_1d(float KalmanState, float KalmanUncertainty, float KalmanInput, float KalmanMeasurement) {
-  KalmanState = KalmanState + dt * KalmanInput;
-  KalmanUncertainty = KalmanUncertainty + dt * dt * 4 * 4;
-  float KalmanGain = KalmanUncertainty * 1 / (1 * KalmanUncertainty + 3 * 3);
+  KalmanState += dt * KalmanInput;
+  KalmanUncertainty += dt * dt * 16;
+  float KalmanGain = KalmanUncertainty * 1 / (KalmanUncertainty + 9);
   KalmanState = KalmanState + KalmanGain * (KalmanMeasurement - KalmanState);
   KalmanUncertainty = (1 - KalmanGain) * KalmanUncertainty;
   Kalman1DOutput[0] = KalmanState;
@@ -74,17 +73,12 @@ void gyro_signals(void) {
   Wire.write(0x43);
   Wire.endTransmission();
   Wire.requestFrom(0x68,6);
-  int16_t GyroX=Wire.read()<<8 | Wire.read();
   int16_t GyroY=Wire.read()<<8 | Wire.read();
-  int16_t GyroZ=Wire.read()<<8 | Wire.read();
-  RateRoll=(float)GyroX/65.5;
   RatePitch=(float)GyroY/65.5;
-  RateYaw=(float)GyroZ/65.5;
   AccX=(float)AccXLSB/4096;
   AccY=(float)AccYLSB/4096;
   AccZ=(float)AccZLSB/4096;
-  AngleRoll=atan(AccY/sqrt(AccX*AccX+AccZ*AccZ))*1/(3.142/180);
-  AnglePitch=-atan(AccX/sqrt(AccY*AccY+AccZ*AccZ))*1/(3.142/180);
+  AnglePitch=-atan(AccX/sqrt(AccY*AccY+AccZ*AccZ))*57.29578;
 }
 
 void pid(void) {
@@ -160,26 +154,20 @@ void setup() {
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
 
-  Serial.begin(57600);
-  pinMode(13, OUTPUT);
-  digitalWrite(13, HIGH);
+  // Serial.begin(57600);
   Wire.setClock(400000);
   Wire.begin();
-  delay(250);
+  delay(500);
   Wire.beginTransmission(0x68);
   Wire.write(0x6B);
   Wire.write(0x00);
   Wire.endTransmission();
-  for (RateCalibrationNumber = 0; RateCalibrationNumber < 2000; RateCalibrationNumber++) {
+  for (RateCalibrationNumber = 0; RateCalibrationNumber < 500; RateCalibrationNumber++) {
     gyro_signals();
-    RateCalibrationRoll += RateRoll;
     RateCalibrationPitch += RatePitch;
-    RateCalibrationYaw += RateYaw;
     delay(1);
   }
-  RateCalibrationRoll /= 2000;
-  RateCalibrationPitch /= 2000;
-  RateCalibrationYaw /= 2000;
+  RateCalibrationPitch /= 500;
   LoopTimer = micros();
 }
 
@@ -188,30 +176,13 @@ void loop() {
   if (dt < MIN_DT) dt = 0.004f; // Tránh dt quá nhỏ
 
   gyro_signals();
-  RateRoll -= RateCalibrationRoll;
   RatePitch -= RateCalibrationPitch;
-  RateYaw -= RateCalibrationYaw;
-  kalman_1d(KalmanAngleRoll, KalmanUncertaintyAngleRoll, RateRoll, AngleRoll);
-  KalmanAngleRoll = Kalman1DOutput[0];
-  KalmanUncertaintyAngleRoll = Kalman1DOutput[1];
   kalman_1d(KalmanAnglePitch, KalmanUncertaintyAnglePitch, RatePitch, AnglePitch);
   KalmanAnglePitch = Kalman1DOutput[0];
   KalmanUncertaintyAnglePitch = Kalman1DOutput[1];
 
   pid();
   driveMotors();
-
-  // Debug (chỉ dùng khi cần)
-  Serial.print("Roll Angle [°]: ");
-  Serial.print(KalmanAngleRoll);
-  Serial.print(" Pitch Angle [°]: ");
-  Serial.print(KalmanAnglePitch);
-  Serial.print(" Throttle: ");
-  Serial.print(throttle);
-  Serial.print(" PWM: ");
-  Serial.print(pwm);
-  Serial.print(" Error: ");
-  Serial.println(error);
 
   while (micros() - LoopTimer < 4000);
   LoopTimer = micros();
